f01200bc45c339a3a0b0b6ff40c313bac14a7363
svdhcn <huanvuhuu92@gmail.com>
2018-10-23 12:58:08 +0700

    Update Edit Bone

--- blender_modifies/__init__.py
+++ blender_modifies/__init__.py
@@ -27,7 +27,7 @@ def Export_Bvh(file_path):
 		if os.path.exists(file_path):
 			os.remove(file_path)		
 		bpy.context.scene.render.fps = 20  # We configure the frame rate		
-		bpy.ops.export_anim.bvh(filepath=file_path, global_scale = 1, frame_start= 1, frame_end= 50, rotate_mode='NATIVE', root_transform_only=True)
+		bpy.ops.export_anim.bvh(filepath=file_path, global_scale = 1, frame_start= 1, frame_end= 6180, rotate_mode='NATIVE', root_transform_only=True)
 		print("Exporting is successful !!!")
 	except:
 		print("Couldn't export file")
@@ -44,7 +44,7 @@ def Get_Data_Key_Frame():
 def Get_Data_Rotation():
 	sce = bpy.context.scene
 	frame_start = 1
-	frame_end = 6181
+	frame_end = 6180
 	ob = bpy.context.object
 	ROTATION_KEY_DATA = []
 	for f in range(frame_start, frame_end):
@@ -68,27 +68,60 @@ def kmeans_clustering_preview(K):
 		plt.plot(centroids[:,0],centroids[:,1],'sm',markersize=8)
 		plt.show()
 
-def kmeans_clustering(K):
+def kmeans_clustering():
+	K = 30
 	data = Get_Data_Rotation()
 	repeat = 1
-	for i in range(repeat):
-		centroids,_ = kmeans(data, K)
-		idx,_ = vq(data,centroids)
-		#print("centroids:", centroids)
-		#plt.scatter(data[:, 0], data[:, 1], c=idx, s=50, cmap='viridis');
-		#plt.plot(centroids[:,0],centroids[:,1],'sm',markersize=8)
-		#plt.show()
-	IPython.embed()
-	sce = bpy.context.scene
+	# for i in range(repeat):
+	centroids,_ = kmeans(data, K)
+	labels,_ = vq(data,centroids)
+	data = [[]]
+	for label in labels:
+		data.append(centroids[label])
+	data.remove([])
+	return data
+
+	#anch = [[]]
+	#for i in range(0, len(data)):
+	#	if i%20 == 0:
+	#		anch.append[data[i]]
+	#bspline
+	#IPython.embed()
+	#print("centroids:", centroids)
+	#plt.scatter(data[:, 0], data[:, 1], c=idx, s=50, cmap='viridis');
+	#plt.plot(centroids[:,0],centroids[:,1],'sm',markersize=8)
+	#plt.show()
+	
+def Edit_data():
+	data = kmeans_clustering()
+	data = np.array(data)
 	frame_start = 1
-	frame_end = 6181
+	frame_end = 6180
+	sce = bpy.context.scene
 	ob = bpy.context.object
-	ROTATION_KEY_DATA = []
 	for f in range(frame_start, frame_end):
 		sce.frame_set(f)
-		for idx in len(centroids):
-			Edit_Rotation_Bone("RightElbow", f, centroids[i][0], centroids[i][1], centroids[i][2])
-			Edit_Rotation_Bone("RightWrist", f, centroids[i][3], centroids[i][4], centroids[i][5])
+		keyFrame = context.scene.frame_current
+		keyInterp = context.user_preferences.edit.keyframe_new_interpolation_type
+		for pbone in ob.pose.bones:
+			lastMode = pbone.rotation_mode
+			pbone.rotation_mode = "XYZ"
+			bpy.ops.object.mode_set(mode='POSE')
+			pbone.bone.select = True
+			if pbone.name == "RightElbow":					
+				pbone.rotation_euler.x = data[f - 1][0]
+				pbone.rotation_euler.y = data[f - 1][1]
+				pbone.rotation_euler.z = data[f - 1][2]	
+			elif pbone.name == "RightWrist":
+				pbone.rotation_euler.x = data[f - 1][3]
+				pbone.rotation_euler.y = data[f - 1][4]
+				pbone.rotation_euler.z = data[f - 1][5]	
+			bpy.context.scene.update()
+			pbone.keyframe_insert(data_path="rotation_euler" ,frame=keyFrame)
+			context.user_preferences.edit.keyframe_new_interpolation_type = keyInterp
+			bpy.ops.object.mode_set(mode='OBJECT')
+			pbone.rotation_mode = lastMode
+	print("Edit done")
 
 def pca_rotation(pca_components):
 	data = Get_Data_Rotation()
