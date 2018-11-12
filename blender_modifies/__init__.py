164205fa4495c78eaaa91336e187f95b8f910165
svdhcn <huanvuhuu92@gmail.com>
2018-11-12 11:30:58 +0700

    Remove Data BVH

--- blender_modifies/__init__.py
+++ blender_modifies/__init__.py
@@ -17,6 +17,9 @@ from fbpca import diffsnorm
 import GPy
 from scipy.interpolate import BSpline
 
+frame_start = 1
+frame_end = 6180
+
 """"""""""""""""""""""""""""" Import and Export BVH file """""""""""""""""""""""""""""
 
 def Import_Bvh(file_path):
@@ -33,7 +36,7 @@ def Export_Bvh(file_path):
 		if os.path.exists(file_path):
 			os.remove(file_path)		
 		bpy.context.scene.render.fps = 20  # We configure the frame rate		
-		bpy.ops.export_anim.bvh(filepath=file_path, global_scale = 1, frame_start= 1, frame_end= 6180, rotate_mode='NATIVE', root_transform_only=True)
+		bpy.ops.export_anim.bvh(filepath=file_path, global_scale = 1, frame_start= frame_start, frame_end= frame_end, rotate_mode='NATIVE', root_transform_only=True)
 		print("Exporting is successful !!!")
 	except:
 		print("Couldn't export file")
@@ -42,9 +45,14 @@ def Export_Bvh(file_path):
 """"""""""""""""""""""""""""" Machine Learning """""""""""""""""""""""""""""""""""""""
 
 def Kmeans_Clustering_Preview(K):
-	data = pca_rotation(2)
-	Display_Data_Rotation(data)
-	repeat = 20
+	data = Pca_Rotation(2)
+
+	plt.plot(data[:,0], data[:,1], 'b^', markersize = 4, alpha = .8)
+	plt.axis('equal')
+	plt.plot()
+	plt.show()
+
+	repeat = 10
 	mindiff = 0.0
 	labels = []
 	Centroids = []
@@ -70,9 +78,9 @@ def Kmeans_Clustering_Preview(K):
 	plt.show()
 
 def Kmeans_Clustering():
-	K = 50
+	K = 100
 	data = Get_Data_Rotation()
-	repeat = 10
+	repeat = 5
 	mindiff = 0.0
 	labels = []
 	Centroids = []
@@ -85,7 +93,6 @@ def Kmeans_Clustering():
 			labels,_ = vq(data,Centroids)
 		elif mindiff > diff:
 			mindiff = diff
-			print("min diff", mindiff)
 			Centroids = centroids
 			labels,_ = vq(data,Centroids)
 	#  end repeat
@@ -94,8 +101,8 @@ def Kmeans_Clustering():
 		data.append(Centroids[label])
 	data.remove([])
 	data = np.array(data)
-	_SmoothData = Bspline_Rotation_Data(labels, data)	
-	return _SmoothData
+	smoothData = Bspline_Rotation_Data(labels, data)	
+	return data
 
 	#anch = [[]]
 	#for i in range(0, len(data)):
@@ -114,30 +121,46 @@ def Bspline_Rotation_Data(labels, data):
 	knots_array = [[]]
 	z = []
 	k = 2
-	for i in range(0, int(len(labels)/30)):
-		z.append(i*30)
-		knots_array.append(data[labels[i*30]])
+	for i in range(0, int(len(labels)/5)):
+		z.append(i*5)
+		knots_array.append(data[labels[i*5]])
 	knots_array.remove([])
 	knots_array = np.array(knots_array)
-	print ("knots_array: ", knots_array)
+	#print ("knots_array: ", knots_array)
 	x1 = knots_array[:, 0]
 	x2 = knots_array[:, 1]
 	x3 = knots_array[:, 2]
 	x4 = knots_array[:, 3]
 	x5 = knots_array[:, 4]
 	x6 = knots_array[:, 5]
+	x7 = knots_array[:, 6]
+	x8 = knots_array[:, 7]
+	x9 = knots_array[:, 8]
+
 	spl_1 = BSpline(z, x1, k)
 	spl_2 = BSpline(z, x2, k)
 	spl_3 = BSpline(z, x3, k)
 	spl_4 = BSpline(z, x4, k)
 	spl_5 = BSpline(z, x5, k)
 	spl_6 = BSpline(z, x6, k)
-	_Smooth_data = []
-	spl_1_ind = []
+	spl_7 = BSpline(z, x7, k)
+	spl_8 = BSpline(z, x8, k)
+	spl_9 = BSpline(z, x9, k)	
+	smooth_data = []
+	#spl_1_data = []
+	#spl_1_ind = []
 	for i in range(0, len(data)):
-		_Smooth_data.append([spl_1(i), spl_2(i), spl_3(i), spl_4(i), spl_5(i), spl_6(i)])
-	_Smooth_data = np.array(_Smooth_data)
-	return _Smooth_data
+		#spl_1_data.append([spl_1(i)])
+		#spl_1_ind.append(i)
+		smooth_data.append([spl_1(i), spl_2(i), spl_3(i), spl_4(i), spl_5(i), spl_6(i), spl_7(i), spl_8(i), spl_9(i)])
+	smooth_data = np.array(smooth_data)
+	
+	#fig, ax = plt.subplots()
+	#ax.plot(spl_1_ind, spl_1_data, 'b-', lw=4, alpha=0.7, label='BSpline')
+	#ax.grid(True)
+	#ax.legend(loc='best')
+	#plt.show()
+	return smooth_data
 
 def Bspline_Location_Data(data):
 	# This funtion do smooth lotation data 
@@ -164,13 +187,14 @@ def Bspline_Location_Data(data):
 	spl_4 = BSpline(z, x4, k)
 	spl_5 = BSpline(z, x5, k)
 	spl_6 = BSpline(z, x6, k)
+
 	_Smooth_data = []
-	spl_1_ind = []
 	for i in range(0, len(data)):
 		_Smooth_data.append([spl_1(i), spl_2(i), spl_3(i), spl_4(i), spl_5(i), spl_6(i)])
 	_Smooth_data = np.array(_Smooth_data)
 	return _Smooth_data	
 	""" this is plot data after smoothly
+	spl_1_ind = []
 	spl_1_ind.append(i)
 	fig, ax = plt.subplots()
 	ax.plot(spl_1_ind, spl_1_data, 'b-', lw=4, alpha=0.7, label='BSpline')
@@ -182,7 +206,6 @@ def Pca_Rotation(pca_components):
 	# This function to loss dimention data
 
 	data = Get_Data_Rotation()
-	Display_Data_Rotation(data)
 	(U, s, Va) = pca(data, pca_components, True)
 	print("U", U.shape, "s", s.shape, "Va", Va.shape)
 	err = diffsnorm(data, U, s, Va)
@@ -197,16 +220,13 @@ def Get_Data_Rotation():
 	# This function to get all data rotation of bones
 
 	sce = bpy.context.scene
-	frame_start = 1
-	frame_end = 6180
 	ob = bpy.context.object
 	ROTATION_KEY_DATA = []
 	for f in range(frame_start, frame_end):
 		sce.frame_set(f)
 		rotation_bone = []
 		for pbone in ob.pose.bones:
-			if pbone.name != "RightShoulder":
-				rotation_bone.extend([pbone.rotation_euler.x, pbone.rotation_euler.y ,pbone.rotation_euler.z])				
+			rotation_bone.extend([pbone.rotation_euler.x, pbone.rotation_euler.y ,pbone.rotation_euler.z])								
 		ROTATION_KEY_DATA.append(rotation_bone)
 	ROTATION_KEY_DATA = np.array(ROTATION_KEY_DATA)
 	return ROTATION_KEY_DATA
@@ -233,33 +253,32 @@ def Get_Data_Location():
 def Edit_Data_Rotation():
 	# This funtion to change rotation data of bone
 	
-	data = Kmeans_Clustering()
-	frame_start = 1
-	frame_end = 6180
+	data = Kmeans_Clustering()	
 	sce = bpy.context.scene
-	ob = bpy.context.object
-	keyInterp = context.user_preferences.edit.keyframe_new_interpolation_type
-	context.user_preferences.edit.keyframe_new_interpolation_type = "BEZIER"
+	ob = bpy.context.object	
 	bpy.ops.object.mode_set(mode='POSE')
 	for f in range(frame_start, frame_end):
+		frameSet = f - 1
 		sce.frame_set(f)
-		keyFrame = context.scene.frame_current		
+		keyFrame = context.scene.frame_current
+		keyInterp = context.user_preferences.edit.keyframe_new_interpolation_type
+		context.user_preferences.edit.keyframe_new_interpolation_type = "BEZIER"
 		for pbone in ob.pose.bones:
 			lastMode = pbone.rotation_mode
-			pbone.rotation_mode = "XYZ"			
+			pbone.rotation_mode = "XYZ"
 			pbone.bone.select = True
-			if pbone.name != "RightElbow":		
-				pbone.rotation_euler.x = data[f - 1][0]
-				pbone.rotation_euler.y = data[f - 1][1]
-				pbone.rotation_euler.z = data[f - 1][2]	
+			if pbone.name == "RightShoulder":
+				pbone.rotation_euler = data[frameSet][0:3]
+			elif pbone.name == "RightElbow":
+				pbone.rotation_euler = data[frameSet][3:6]
 			elif pbone.name == "RightWrist":
-				pbone.rotation_euler.x = data[f - 1][3]
-				pbone.rotation_euler.y = data[f - 1][4]
-				pbone.rotation_euler.z = data[f - 1][5]	
+				pbone.rotation_euler = data[frameSet][6:9]
+			
 			bpy.context.scene.update()
-			pbone.keyframe_insert(data_path="rotation_euler" ,frame=keyFrame)			
+			pbone.keyframe_insert(data_path="rotation_euler" ,frame=keyFrame)
 			pbone.rotation_mode = lastMode
-	context.user_preferences.edit.keyframe_new_interpolation_type = keyInterp
+			context.user_preferences.edit.keyframe_new_interpolation_type = keyInterp
+			#IPython.embed()
 	bpy.ops.object.mode_set(mode='OBJECT')
 	print("Edit rotation done")
 
@@ -324,7 +343,7 @@ def Edit_Rotation_Bone( BoneName, FrameNumber, ValueX, ValueY, ValueZ, bdegrees 
 	sce = bpy.context.scene
 	ob = bpy.context.object
 
-	for f in range(sce.frame_start, sce.frame_end+1):
+	for f in range(sce.frame_start, sce.frame_end + 1):
 		sce.frame_set(f)
 		if f == FrameNumber:
 			keyFrame = context.scene.frame_current
@@ -348,9 +367,3 @@ def Edit_Rotation_Bone( BoneName, FrameNumber, ValueX, ValueY, ValueZ, bdegrees 
 					context.user_preferences.edit.keyframe_new_interpolation_type = keyInterp
 					bpy.ops.object.mode_set(mode='OBJECT')
 					pbone.rotation_mode = lastMode
-
-def Change_Lotation_Data():
-	_LocationData = Get_Data_Location()
-	return Bspline_Data(_LocationData)
-
-""""""""""""""""""""""""""""""" To be continued !!! """"""""""""""""""""""""""""""""""
--- blender_modifies/runBlender.py
+++ blender_modifies/runBlender.py
@@ -1,9 +1,9 @@
 import blender_modifies
 
-blender_modifies.Import_Bvh("/home/huan/Documents_Master/Style_Learning/two_cmu_retargeted/Export_Right_Hand_9dimention.bvh")
-#blender_modifies.Get_Data_Rotation()
-blender_modifies.Edit_Data_Rotation()
+blender_modifies.Import_Bvh("/home/huan/Documents_Master/Style_Learning/two_cmu_retargeted/Right_Hand_SuyVan.bvh")
+blender_modifies.Kmeans_Clustering_Preview(80)
+#blender_modifies.Edit_Data_Rotation()
 #blender_modifies.kmeans_clustering_preview(50)
 #blender_modifies.isolate_ingredient_data()
 #blender_modifies.Edit_data()
-blender_modifies.Export_Bvh("/home/huan/Documents_Master/Style_Learning/two_cmu_retargeted/Export_Right_Hand_ver1.bvh")
+#blender_modifies.Export_Bvh("/home/huan/Documents_Master/Style_Learning/two_cmu_retargeted/Right_Hand_SuyVan_Export.bvh")
