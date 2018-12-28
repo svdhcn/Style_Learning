df3d4e53c3862b4f4e76409b84f994fcc2cd3963
KHMT <khmt@ubuntu.ubuntu-domain>
2018-12-28 17:33:48 +0700

    update

--- startup/HMI_ManagementData.py
+++ startup/HMI_ManagementData.py
@@ -4,10 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import glob, os
 import numpy as np
-import pandas as pd
-
-List_Bones_UpperBody = ['Hips','Chest', 'Chest2', 'Chest3', 'Chest4', 'Neck', 'Head', 'RightCollar', 'RightShoulder', 'RightElbow', 'RightWrist', 'LeftCollar', 'LeftShoulder', 'LeftElbow', 'LeftWrist', 'RightHip', 'RightKnee', 'RightAnkle', 'RightToe', 'LeftHip', 'LeftKnee', 'LeftAnkle', 'LeftToe']
-		
+import pandas as pd		
 
 def get_text_files_data():
 	os.getcwd()
@@ -19,21 +16,29 @@ def get_text_files_data():
 		text_data.append(path + '/'+ file)
 	return text_data
 
-def write_data_to_dataFrame():
+def update_data_to_dataFrame():
 	List_Bones = ['Hips','Chest', 'Chest2', 'Chest3', 'Chest4', 'Neck', 'Head',
 				'RightCollar', 'RightShoulder', 'RightElbow', 'RightWrist', 'LeftCollar', 'LeftShoulder', 'LeftElbow', 'LeftWrist',
 				'RightHip', 'RightKnee', 'RightAnkle', 'RightToe', 'LeftHip', 'LeftKnee', 'LeftAnkle', 'LeftToe']
 	
-	Bones_Body = np.array(List_Bones)
-	DataMotion = pd.DataFrame(columns= Bones_Body)
+	Bones_Body = np.array(List_Bones)	
 	fname = ''
 	textData = get_text_files_data()
+	df = pd.DataFrame(columns= List_Bones)
 	for data in textData:
 		fname = data.split('\\')[-1:][-1]
-		f = open(data, 'r' )
-		for i in range(0,50):
-			s = f.readline()
+		filename_w_ext = os.path.basename(fname)
+		filename, file_extension = os.path.splitext(filename_w_ext)
+		with open(data, encoding="utf-8") as file:
+			x = [l.strip() for l in file]
+			X = np.array(x)			
+			for data in X:
+				dataMotion  = np.array(data)
+				IPython.embed()			
+				
+				newdf = pd.DataFrame(newdata, columns= List_Bones)
+				df.append(newdf)
 			IPython.embed()
 	return {'FINISHED'}
 
-d = write_data_to_dataFrame()
+d = update_data_to_dataFrame()
